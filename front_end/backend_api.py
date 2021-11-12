c9a24a50193021d434733d8d959e5c312296e147
414dna <48360598+414dna@users.noreply.github.com>
2021-11-12 17:01:27 +0530

    Add files via upload

---
+++ front_end/backend_api.py
@@ -0,0 +1,28 @@
+
+import pyrebase
+token=0
+config = {
+  "apiKey": "AIzaSyCJRE8-9fsXIlj6O5yjmH6xjl62I3wgrD0",
+  "authDomain": "first-baby-4aabf.firebaseapp.com",
+  "databaseURL": "https://first-baby-4aabf-default-rtdb.firebaseio.com", 
+  "storageBucket": "first-baby-4aabf.appspot.com",
+  "messagingSenderId": "488420073997",
+  "appId": "1:488420073997:web:f18907e3a1bd72559bab44",
+  "measurementId": "G-MB5Y9K2R7L"
+}
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+auth = firebase.auth()
+def auth1(email=None,password=None):
+  user = auth.sign_in_with_email_and_password(email,password)
+  print(user)
+  return user
+def user1(node=None,id=None):
+  data = {"humid": 0,'id':id,'temp': 0}
+  db.child(node).child(id).set(data)
+  Keys = db.child(node).shallow().get().val()
+  for key in Keys:
+    if key.isnumeric():
+      value=db.child(node).child(key).get()
+      print(value.val()) 
+      return 'success'
---
+++ front_end/front_end.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template,request,flash
+from requests.models import HTTPError
+from backend_api import auth1, user1
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('front.html')
+@app.route('/confirm',methods=['POST','GET'])
+def confirm():
+    if request.method=='POST':
+        # n=request.form.get('uname')
+        mail=request.form.get('mail')
+        password=request.form.get('psw')
+        print(f'/{mail}/{password}')
+        try:
+            resp=auth1(email=mail,password=password)
+        except HTTPError:
+            flash('Invalid password provided error')
+            return 'efef'
+        if resp['registered']:
+            # user1(node=1,id=6)
+            return render_template('nodepg.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
